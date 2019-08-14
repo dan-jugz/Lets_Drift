5a073cf867733bcf1cf007a9bf363590b1ff08da
Sunday Brian <brians931@gmail.com>
2019-08-14 11:45:58 +0300

    add the profile update view function

--- app/main/views.py
+++ app/main/views.py
@@ -44,4 +44,41 @@ def profile(uname):
 
     return render_template('profile/user_profile.html',user=user)   
 
+    
+@main.route('/user/<uname>/profile_update',methods=['GET','POST'])
+@login_required
+def profile_update(uname):
+    '''
+    view function to updtae a user profile
+    '''
+    user=User.query.filter_by(username=uname).first()
+    if user is None:
+        abort(404)
+    
+    print(user)    
 
+    
+    form=UpdateProfileForm()
+
+    if form.validate_on_submit():
+        
+        if form.pictures.data:
+            pic=add_profile_pic(form.picture.data,user.username)
+            current_user.profile_image=pic
+        user.username=form.username.data
+        user.email=form.email.data
+
+        db.session.add(user) 
+        db.session.commit() 
+
+        return redirect(url_for('main.profile',uname=user.username)) 
+
+    elif request.method=='GET':
+        #user is not submitting anything
+        #set form field to the current users details
+        form.username.data=current_user.username
+        form.email.data=current_user.email
+    
+    #fetch the current profile image from the static folder and inject to the template    
+    profile_image=url_for('static',filename='profile_pics/'+user.profile_image)  
+    return render_template('profile/update_profile.html',form=form)  
