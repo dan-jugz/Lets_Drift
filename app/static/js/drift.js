0093786ebbe05747ccc845bb13c789c5473f17ee
Sunday Brian <brians931@gmail.com>
2019-08-15 22:56:43 +0300

    styled the profile page card

--- app/static/js/drift.js
+++ app/static/js/drift.js
@@ -2,11 +2,25 @@
 
  ///////////////////////////
   // Preloader
-  $(window).on('load', function () {
+//   $(window).on('load', function () {
 
-    $("#loader").fadeOut("slow", function() {
-    // will fade out the whole DIV that covers the website.
-    $("#preloader").delay(1200).fadeOut("slow");
-    });
+//     $("#loader").fadeOut("slow", function() {
+//     // will fade out the whole DIV that covers the website.
+//     $("#preloader").delay(1200).fadeOut("slow");
+//     });
 
-  });
+//   });
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+
